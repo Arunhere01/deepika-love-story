bf0af0b4ac05e2e892ea79f69360c615abbaaf34
Arun <arun.here01@gmail.com>
2025-07-09 11:48:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+
+function next() {
+    const name = document.getElementById('nameInput').value || "Deepika";
+    alert("Shall we go on a ride of memories, " + name + "?");
+    // You can implement redirection to album slides here
+}
