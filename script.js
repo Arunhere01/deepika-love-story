b52b2ec2715c2d15e80c31f1fb9dbe422538d0d3
Arun <arun.here01@gmail.com>
2025-07-09 12:23:53 +0530

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-
-function next() {
-    const name = document.getElementById('nameInput').value || "Deepika";
-    alert("Shall we go on a ride of memories, " + name + "?");
-    // You can implement redirection to album slides here
-}
